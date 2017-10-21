2e02ab3317853819516260e6ed92433574d75763
Ethan Hall <ethan@ehdev.io>
2017-10-20 18:02:05 -0700

    Fixing the tag script

--- scripts/tag-version.sh
+++ scripts/tag-version.sh
@@ -15,6 +15,9 @@ MESSAGE=`git log --format=%B -n 1 HEAD`
 SHA=`git rev-list HEAD | head -n 1`
 SUBJECT=`git log --format=%s -n 1 HEAD`
 COMMIT_BODY=`git log --format=%b -n 1 HEAD`
+AUTHOR_NAME=`git log --format=%an -n 1 HEAD`
+AUTHOR_EMAIL=`git log --format=%ae -n 1 HEAD`
+COMMIT_TIME=`git log --format=%cI -n 1 HEAD`
 
 BODY=`jq --null-input --argjson commits "$COMMITS" --arg message "${MESSAGE}" --arg ID "${SHA}" '{ "commits": $commits, "message": $message, "commitId": $ID}'`
 
@@ -22,19 +25,24 @@ echo "${BODY}" | curl -s -X POST -H "X-AUTH-TOKEN: ${AUTH_TOKEN}" -H "Content-Ty
 
 VERSION=`curl -s -H "Content-Type: application/json" http://api.crom.tech/api/v1/project/ethankhall/repo/inc/version/$SHA | jq -r '.version'`
 
-jq --null-input --arg MESSAGE "${MESSAGE}" --arg ID "${ID}" --arg EMAIL "${EMAIL}" 
+TAG_BODY=`jq --null-input --arg SUBJECT "${SUBJECT}" \
+  --arg SHA "${SHA}" \
+  --arg AUTHOR_NAME "${AUTHOR_NAME}" \
+  --arg AUTHOR_EMAIL "${AUTHOR_EMAIL}" \
+  --arg COMMIT_TIME "${COMMIT_TIME}" \
+  --arg VERSION "${VERSION}" \
+  $'{ "message": $SUBJECT, 
+      "object": $SHA, 
+      "type": "commit", 
+      "tag": ("v" + $VERSION),
+      "tagger": { 
+        "name": $AUTHOR_NAME, 
+        "email": $AUTHOR_EMAIL, 
+        "date": $COMMIT_TIME
+      }
+    }'``
 
 curl -u ethankhall:$GITHUB_API_TOKEN -X "POST" \
     "https://api.github.com/repos/ethankhall/inc/git/tags?name=inc-darwin-0.1.3" \
      -H "Content-Type: application/json; charset=utf-8" \
-     -d $'{
-  "message": "initial version\\n",
-  "object": "c3d0be41ecbe669545ee3e94d31ed9a4bc91ee3c",
-  "tagger": {
-    "name": "Scott Chacon",
-    "email": "schacon@gmail.com",
-    "date": "2011-06-17T14:53:35-07:00"
-  },
-  "type": "commit",
-  "tag": "v0.0.1"
-}'
+     -d "$TAG_BODY"
